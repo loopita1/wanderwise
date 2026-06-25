d0ff709ca9aa9e8e5324ba4d0d5e90855aaa1a8b
Diana Calderon <dianacalderon2005@gmail.com>
2026-06-25 03:17:40 +0000

    preparing for database

--- unit_tests.py
+++ unit_tests.py
@@ -1,18 +1,18 @@
 import unittest
-from gen import valid_date
+from gen import validDate
 
 class TestDate(unittest.TestCase):
 
-    def test_valid_date(self):
+    def testValidDate(self):
         self.assertFalse(valid_date("06/24/2026"))
     
-    def test_invalid_format(self):
+    def testInvalidFormat(self):
         self.assertTrue(valid_date("04-20-2026"))
 
-    def test_invalid_date(self):
+    def testInvalidDate(self):
         self.assertTrue(valid_date("16/24/2026"))
     
-    def test_string(self):
+    def testString(self):
         self.assertTrue(valid_date("hi"))
 
 if __name__ == "__main__":
