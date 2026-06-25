86008d7dba7b6ca59491c1dd8a7f90e21a164ecc
Diana Calderon <dianacalderon2005@gmail.com>
2026-06-25 20:28:09 +0000

    work

--- gen.py
+++ gen.py
@@ -10,40 +10,41 @@ def validDate(dates):
         except ValueError:
             return True
 
-if __name__ == "__main__":
-    print("Welcome to WanderWise, your personal travel assistant!")
-    answer = input("Do you already have a place and date in mind? (Y/N)\n")
-    load_dotenv(dotenv_path=".env")
+def genAI():
+        print("Welcome to WanderWise, your personal travel assistant!")
+        answer = input("Do you already have a place and date in mind? (Y/N)\n")
+        load_dotenv(dotenv_path=".env")
 
-    api_key = os.getenv("GEMINI_API_KEY")
-
-    while answer.lower() != "y" and answer.lower() != "n":
-        answer = input("Do you already have a place and date in mind?")
-    if answer.lower() == "n":
-        
-        interaction = client.interactions.create(
-            model="gemini-2.5-flash-lite",
-            input=f"give a list of the 5 interesting facts for tourists to know for each country in {top3}"
-        )
-        print(interaction.output_text)
-    else:
-        location = input("Please put your desired city and country of origin, separating them with a comma (Ex: Madrid, Spain)\n")
-
-        # check for valid start and end dates
-        start = input("Please enter your intended arrival date for travel in the format MM/DD/YY\n")    
-        while validDate(start):
-            start = input("Please enter your intended arrival date for travel in the format MM/DD/YY\n")    
-        start_date = datetime.strptime(start, "%m/%d/%Y")
-
-        end = input("Please enter your intended departure date for travel in the format MM/DD/YY\n")
-        end_date = datetime.strptime(end, "%m/%d/%Y")
-        while validDate(end) or end_date <= start_date:
-            end = input("Please enter your intended departure date for travel in the format MM/DD/YY\n") 
+        api_key = os.getenv("GEMINI_API_KEY")
         client = genai.Client(api_key=api_key)
 
-        # get and print gen ai response
-        interaction = client.interactions.create(
-            model="gemini-2.5-flash-lite",
-            input=f"give a day by day itinerary of {location} for a tourist to go during {start} until {end} that are popular during the season, make sure the itinerary is clear a bulleted with dates and locations, also provide a packing list below this itinerary including the clothing to wear, the type of outlet the location has, currency, overall weather during the dates, and the primary language spoken"
-        )
-        print(interaction.output_text)
+        while answer.lower() != "y" and answer.lower() != "n":
+            answer = input("Do you already have a place and date in mind?")
+        if answer.lower() == "n":
+
+            interaction = client.interactions.create(
+                model="gemini-2.5-flash-lite",
+                input=f"give a list of the 5 interesting facts for tourists to know for each country in {top3}"
+            )
+            print(interaction.output_text)
+            
+        else:
+            location = input("Please put your desired city and country of origin, separating them with a comma (Ex: Madrid, Spain)\n")
+
+            # check for valid start and end dates
+            start = input("Please enter your intended arrival date for travel in the format MM/DD/YY\n")    
+            while validDate(start):
+                start = input("Please enter your intended arrival date for travel in the format MM/DD/YY\n")    
+            start_date = datetime.strptime(start, "%m/%d/%Y")
+
+            end = input("Please enter your intended departure date for travel in the format MM/DD/YY\n")
+            end_date = datetime.strptime(end, "%m/%d/%Y")
+            while validDate(end) or end_date <= start_date:
+                end = input("Please enter your intended departure date for travel in the format MM/DD/YY\n") 
+            print("Please hold while we get your itinerary!")
+            # get and print gen ai response
+            interaction = client.interactions.create(
+                model="gemini-2.5-flash-lite",
+                input=f"give a day by day itinerary of {location} for a tourist to go during {start} until {end} that are popular during the season, make sure the itinerary is clear a bulleted with dates and locations, also provide a packing list below this itinerary including the clothing to wear, the type of outlet the location has, currency, overall weather during the dates, and the primary language spoken"
+            )
+            print(interaction.output_text)
---
+++ main.py
@@ -0,0 +1,4 @@
+from gen import genAI
+
+if __name__ == "__main__":
+    genAI()
